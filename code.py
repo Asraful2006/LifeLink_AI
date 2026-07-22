f97c40fcfc1fcceef707bd7c5467e07f0da94aa2
Asraful Hossan Shyket <asrafulhossanshyket58@gmail.com>
2026-07-23 00:16:26 +0600

    added lifelink project

--- code.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-for i in range(n):
-    word = input()
-    if len(word)>10 :
-        result= word[0]+ str(len(word)-2) + word[-1]
-        print(result)
-    else:
-        print(word)    
-
--- hello.py
+++
@@ -1,8 +0,0 @@
-#nested loop
-
-for i in range(5):
-    sum = 0
-    for j in range(1, i+2):
-        sum = sum + j
-        print(j, end = " ")
-    print(" = ", sum)
